a0d1cb5b22e66d68a4df09456c8a12c3ed5e1577
Melissa Danielle Myatt <mdm@Melissas-Air.attlocal.net>
2021-04-06 17:06:48 -0700

    updated api.js file

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
+const BASE_URL = process.env.REACT_APP_BASE_URL || "https://react-jobly-2.herokuapp.com/";
 
 /** API Class.
  *
